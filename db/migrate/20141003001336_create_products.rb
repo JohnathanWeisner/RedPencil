e4fca27ef68e96b951df8e357303322191874534
Johnathan Weisner <joweisner@gmail.com>
2014-10-02 21:22:44 -0400

    Add Products

--- db/migrate/20141003001336_create_products.rb
+++ db/migrate/20141003001336_create_products.rb
@@ -4,6 +4,7 @@ class CreateProducts < ActiveRecord::Migration
       t.string :name
       t.text :description
       t.integer :price
+      t.boolean :red_pencil_tag
 
       t.timestamps
     end
